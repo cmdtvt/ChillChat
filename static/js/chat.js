0cbf4fd3b1d353734084bc7fee8a1d023e5e55e0
cmdtvt <tvtuusa@gmail.com>
2021-08-29 22:32:00 +0300

    React layout system setup

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,41 @@
-ReactDOM.render(
-    <h1>React go brrrrrrrrr</h1>,
-    document.getElementById('root')
+console.log("React runs!");
+
+ReactDOM.render( 
+    <App />,
+    document.getElementById('main')
+
 );
+
+//Manages the canvas and layouts are loaded into it.
+function App() {
+
+    var userdata = {
+        layout : {
+
+
+            settings: {
+                maxSize : 12,
+            },
+
+            layout: [{size:2,widget:"userinfo"}]
+        },
+
+        username: 'TeroTesti',
+    }
+
+    console.log(userdata['layout']['layout']);
+    var state = {}
+
+    return (
+        <div className="App">
+            {userdata['layout']['layout'].map((grid) => (<p>{grid.size}</p>))}
+        </div>
+    )
+
+}
+
+function LayoutItem() {
+    return (
+        <div>This is a layout item.</div>
+    )
+}
