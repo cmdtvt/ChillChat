ae235f14616de368c27773a4eb4ba04358ae90b5
cmdtvt <tvtuusa@gmail.com>
2021-08-30 20:35:42 +0300

    Changed chat.js to use classes.

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,67 +1,87 @@
-console.log("React runs!");
+class ChatApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props
+        this.themeData = {
+            layout : {
+                settings: {
+                    maxSize : 12,
+                },
+    
+                widLayout: [
+                    {id:0,size:2,widget:"userinfo"},
+                    {id:1,size:2,widget:"userinfo"},
+                    {id:2,size:2,widget:"userinfo"},
+                    {id:3,size:2,widget:"userinfo"},
+                    {id:4,size:2,widget:"userinfo"},
+                    {id:5,size:2,widget:"userinfo"}
+                ]
+            },
+    
+            styles : {
+    
+            }
+        }
+
+        console.log(this.themeData['layout']['widLayout']);
+        var state = {} //Dont shove everything here!
+    
+        this.LayoutItemList = [];
+        for (let i of this.themeData['layout']['widLayout']) {
+            var text = i.widget;
+            var data = i
+            this.LayoutItemList.push(<LayoutItem widgetData={data}/>);
+        }
+
+        this.state = {}
+    }
+
+
+    render() {
+        return(
+            <div className="container-full ChatApp">
+                {this.LayoutItemList}
+            </div>
+        )
+    }
+}
+
+
+class LayoutItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.props = props
+    }
+
+    render() {
+        return(
+            <div className="item layout-item">
+                Widgetname:{this.props.widgetData.widget}
+            </div>
+        )
+    }
+}
+
+class WidgetWrapper extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.props = props
+    }
+
+    render() {
+        return (
+            <div className="widge">
+                adadadadadad
+            </div>
+        )
+    }
+}
+
 
 ReactDOM.render( 
-    
     <ChatApp/>,
     document.getElementById('main')
 );
-
-//Manages the canvas and layouts are loaded into it.
-function ChatApp() {
-
-    var themeData = {
-        layout : {
-            settings: {
-                maxSize : 12,
-            },
-
-            widLayout: [
-                {id:0,size:2,widget:"userinfo"},
-                {id:1,size:2,widget:"userinfo"},
-                {id:2,size:2,widget:"userinfo"},
-                {id:3,size:2,widget:"userinfo"},
-                {id:4,size:2,widget:"userinfo"},
-                {id:5,size:2,widget:"userinfo"}
-            ]
-        },
-
-        styles : {
-
-        }
-    }
-
-    console.log(themeData['layout']['widLayout']);
-    var state = {} //Dont shove everything here!
-
-    var LayoutItemList = [];
-    for (let i of themeData['layout']['widLayout']) {
-        var text = i.widget;
-        LayoutItemList.push(<LayoutItem widgetName={text}/>);
-    }
-
-    return (
-        <div className="container-full ChatApp">
-            {LayoutItemList}
-        </div>
-    )
-
-}
-
-function LayoutItem(props) {
-    return (
-        <div className="item layout-item">
-            Data from parent is:{props.widgetName}
-        </div>
-    )
-}
-
-//Inherited by widgets so they know how to scale etc...
-function WidgetWrapper() {
-    return (
-        <div className="widge">
-            adadadadadad
-        </div>
-    )
-}
-
 
