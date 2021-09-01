e5c10712fc6dc045ea2f3096c61829eec1c03276
dss285 <aeonmoans@gmail.com>
2021-09-01 19:45:02 +0300

    Endpoints

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Quart(__name__, static_folder="static")
 app.register_blueprint(api_blueprint, url_prefix="/v1")
 @app.route('/')
 async def hello():
-    return await render_template("views/home.html", )
+    return await render_template("views/home.html")
 
 @app.route('/chat')
 async def chat():
